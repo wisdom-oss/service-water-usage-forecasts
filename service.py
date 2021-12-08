178c7feaf6d2f8542e72fb8138424ed2a61de32d
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2021-12-08 14:55:44 +0100

    Added a signal handler for handling docker shutdowns gracefully

--- service.py
+++ service.py
@@ -1,23 +1,33 @@
 """This file will bootstrap the service and announce it at the service registry"""
 import logging
 import os
+import signal
 
 import py_eureka_client.eureka_client
 import pydantic.error_wrappers
 
 from data_models import ServiceSettings
+from messaging.reconnecting_consumer import ReconnectingAMQPConsumer
+
+# Configure the logger for this service
+logger_level_raw = os.getenv('LOG_LEVEL', 'INFO')
+logger_level = getattr(logging, logger_level_raw.upper())
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(name)s - %(funcName)s - %(lineno)s - %(message)s',
+    level=logger_level,
+    force=True
+)
+logger = logging.getLogger('BOOTSTRAP')
+
+
+def docker_signal_handler(sig, frame):
+    logger.info("Detected shutdown signal!")
+    raise KeyboardInterrupt
+
+
+signal.signal(signal.SIGTERM, docker_signal_handler)
 
 if __name__ == '__main__':
-    # Configure the logger for this service
-    logger_level_raw = os.getenv('LOG_LEVEL', 'INFO')
-    logger_level = getattr(logging, logger_level_raw.upper())
-    logging.basicConfig(
-        format='%(asctime)s - %(levelname)s - %(name)s - %(funcName)s - %(lineno)s - %(message)s',
-        level=logger_level,
-        force=True
-    )
-    logger = logging.getLogger('BOOTSTRAP')
-
     # Try reading the current settings
     settings = None
     try:
