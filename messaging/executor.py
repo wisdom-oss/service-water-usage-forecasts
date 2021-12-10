2fc233984bf28b0bed1f301e31cd1918c21c1d7c
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2021-12-10 18:54:46 +0100

    Added polynomial regression

--- messaging/executor.py
+++ messaging/executor.py
@@ -2,7 +2,11 @@
 import logging
 import numpy as np
 from sklearn.linear_model import LinearRegression
-from data_models.forecasts import ForecastData, ForecastRequest, ForecastResponse, ForecastType, \
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+import matplotlib.pyplot as plt
+from data_models.forecasts import ConsumerGroup, ForecastData, ForecastRequest, ForecastResponse, \
+    ForecastType, \
     RealData
 
 __logger = logging.getLogger(__name__)
@@ -29,32 +33,46 @@ def execute(message: dict) -> dict:
     # Create a numpy array for the x-Axis
     x = np.arange(
         request.time_period_start, stop=request.time_period_end + 1
-    ).reshape((-1, 1))
-    print(x)
+    )
+    # Create a new array for years to predict outgoing from the end of the available data
+    x_to_predict = np.arange(
+        request.time_period_end + 1, stop=request.time_period_end + 1 + 10
+    )
     # Create an array for the y-Axis
     water_usage_amounts = np.array(request.water_usage_amounts)
-    if request.forecast_type == ForecastType.LINEAR_FORECAST:
+    if request.forecast_type == ForecastType.LINEAR:
         __logger.info('Running linear forecast of dataset')
         # Initialize the regression model
         model = LinearRegression()
         # Run the linear regression
-        model.fit(x, water_usage_amounts)
-        # Create a new array for years to predict outgoing from the end of the available data
-        x_to_predict = np.arange(
-            request.time_period_end + 1, stop=request.time_period_end + 1 + 10
-        ).reshape((-1, 1))
-        print(x_to_predict)
+        model.fit(x.reshape((-1, 1)), water_usage_amounts)
         data = {
             "forecast_starts": request.time_period_end + 1,
             "forecast_period": 10,
             "forecast_equation": f"y = {model.coef_[0]} * x + {model.intercept_}",
-            "forecast_values": model.predict(x_to_predict).tolist(),
-            "forecast_score": model.score(x, water_usage_amounts)
+            "forecast_values": model.predict(x_to_predict.reshape((-1, 1))).tolist(),
+            "forecast_score": model.score(x.reshape((-1, 1)), water_usage_amounts)
         }
-        print(data)
         return ForecastResponse(
-            forecast_type=ForecastType.LINEAR_FORECAST,
+            forecast_type=ForecastType.LINEAR,
             base_data=RealData.parse_obj(request.dict(exclude={"forecast_type"})),
             prediction_data=ForecastData.parse_obj(data)
         ).dict(by_alias=True)
-
+    elif request.forecast_type == ForecastType.POLYNOMIAL:
+        __logger.info('Running polynomial forecast of the dataset')
+        poly = np.polyfit(x, water_usage_amounts, 2)
+        predicted_data = np.poly1d(poly)(x_to_predict)
+        data = {
+            "consumer_group": ConsumerGroup.HOUSEHOLDS_AND_SMALL_BUSINESSES,
+            "forecast_starts":   request.time_period_end + 1,
+            "forecast_period":   10,
+            "forecast_equation": f"y = {poly[0].tolist()} * x^2 + {poly[1].tolist()} * x +"
+                                 f" {poly[2].tolist()}",
+            "forecast_values":   predicted_data.tolist(),
+            "forecast_score":    np.corrcoef(water_usage_amounts[:10], predicted_data)[0, 1]**2
+        }
+        return ForecastResponse(
+            forecast_type=ForecastType.POLYNOMIAL,
+            base_data=RealData.parse_obj(request.dict(exclude={"forecast_type"})),
+            prediction_data=ForecastData.parse_obj(data)
+        ).dict(by_alias=True)
