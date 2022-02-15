ce926f040c3a711ee226e120256b81055be31b97
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-02-15 12:43:35 +0100

    Unified the needed environment variables

--- data_models/__init__.py
+++ data_models/__init__.py
@@ -7,27 +7,19 @@ class ServiceSettings(BaseSettings):
     Settings this service will use throughout the execution
     """
 
-    eureka_hostname: constr(strip_whitespace=True, to_lower=True, min_length=1) = Field(
+    eureka_hostname: str = Field(
         # Require this setting to be set
         default=...,
         # Name of the environment variable which will be read
-        env="EUREKA_HOSTNAME"
+        env="SERVICE_REGISTRY_HOST"
     )
     """
     Hostname of the eureka service registry at which the instance will announce itself as available
     """
 
-    eureka_port: conint(gt=0, lt=65536) = Field(
-        default=8761,
-        env="EUREKA_PORT"
-    )
-    """
-    Port of the eureka installation, defaults to port 8761
-    """
-
     amqp_url: stricturl(tld_required=False, allowed_schemes={"amqp"}) = Field(
         default=...,
-        env="AMQP_URL"
+        env="AMQP_DSN"
     )
     """Connection URL for the message broker"""
 
