bbb259e0b4a23d09910cb7b31757a87222ca77d4
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-03-27 17:54:37 +0200

    Transitioned to the amqp-rpc-server

--- settings/__init__.py
+++ settings/__init__.py
@@ -1,109 +1,112 @@
-"""Module containing all settings which are used throughout the whole application"""
-from pydantic import BaseSettings, AmqpDsn, stricturl, Field
+"""Settings for the service"""
+import pika.exchange_type
+import pydantic
 
 
-class ServiceSettings(BaseSettings):
-    """Settings related to the general operation of the service"""
+class ServiceSettings(pydantic.BaseSettings):
+    """Settings for the general execution of this service"""
     
-    name: str = Field(
-        default='water-usage-forecast-calculations',
-        title='Service Name',
-        description='The name of the service which is used for registering at the service '
-                    'registry and for authorization in AMQP requests/responses',
+    name: str = pydantic.Field(
+        default='amqp-authorization-service',
+        title='Application Name',
+        description='The name of the service which will be used when registering the service at '
+                    'the service registry',
         env='SERVICE_NAME'
     )
     """
-    Service Name
+    Application Name
     
-    The name of the service which is used for registering at the service registry and for
-    authorization in AMQP responses
+    The name of the microservice which is used for registering at the service registry
     """
-    
-    log_level: str = Field(
+
+    log_level: str = pydantic.Field(
         default='INFO',
         title='Logging Level',
-        description='The level of logging which the root logger will be configured for',
+        description='The level of logging which the root logger will use',
         env='SERVICE_LOG_LEVEL'
     )
     """
     Logging Level
-    
-    The level of logging which will displayed during the service execution
-    """
-    
-    class Config:
-        """Configuration of the Service Settings"""
-        
-        env_file = '.service.env'
-        """The location of the environment file from which the settings may be read"""
-        
 
-class ServiceRegistrySettings(BaseSettings):
-    """Settings related to the connection to the service registry"""
-    
-    host: str = Field(
-        default=...,
-        title='Service Registry Host',
-        description='The hostname or ip address of the service registry on which the service will '
-                    'register itself',
-        env='SERVICE_REGISTRY_HOST'
+    The level of logging which will be used by the root logger
+    """
+
+    log_format: str = pydantic.Field(
+        default='%(levelname)-8s | %(asctime)s | %(name)s | %(message)s',
+        title='Logging Format',
+        description='The format of logging which the root logger will use',
+        env='SERVICE_LOG_FORMAT'
     )
     """
-    Service Registry Host [REQUIRED]
-    
-    The hostname or IP address of the host on which the service registry runs.
+    Logging Format
+
+    The format which will be used by the root logger
     """
-    
-    port: int = Field(
-        default=8761,
-        title='Service Registry Port',
-        description='The port on which the service registry listens, defaults 8761',
-        env='SERVICE_REGISTRY_PORT'
-    )
-    """
-    Service Registry Port [OPTIONAL, default value: `8761`]
-    
-    The port on which the service registry listens
-    """
-    
+
     class Config:
-        """The configuration of hte service registry settings"""
-        
-        env_file = '.registry.env'
-        """The location of the environment file from which the settings may be read"""
-
-
-class AMQPSettings(BaseSettings):
-    """Settings related to the AMQP connection and the communication"""
+        """Configuration of the service settings"""
     
-    dsn: AmqpDsn = Field(
+        env_file = 'env\\.application.env'
+        """Allow loading the values for the service settings from the specified file"""
+
+
+class AMQPSettings(pydantic.BaseSettings):
+    """Settings related to the connection to a AMQPv0-9-1 compatible message broker"""
+    
+    dsn: pydantic.AmqpDsn = pydantic.Field(
         default=...,
-        title='AMQP Data Source Name',
-        description='The URI pointing to the installation of a AMQP-0-9-1 compatible message '
-                    'broker',
+        alias='AMQP_DSN',
+        title='Data Source Name',
+        description='A data source name pointing to a message broker supporting the AMQPv0-9-1 '
+                    'protocol',
         env='AMQP_DSN'
     )
     """
-    AMQP Data Source Name [REQUIRED]
+    Data Source Name
     
-    The URI pointing to the installation of a AMQP-0-9-1 compatible message broker
+    A URI pointing to a message broker. The message broker needs to implement the version 0-9-1
+    of the Advanced Message Queuing Protocol.
     """
     
-    exchange: str = Field(
-        default='water-usage-forecasts',
-        title='AMQP Exchange Name',
-        description='The name of the AMQP exchange this service listens to for messages',
-        env='AMQP_EXCHANGE'
+    exchange_name: str = pydantic.Field(
+        default=...,
+        alias='AMQP_EXCHANGE_NAME',
+        title='Message Broker Exchange Name',
+        description='The name of the exchange the service will bind itself to for receiving '
+                    'messages',
+        env='AMQP_EXCHANGE_NAME'
     )
     """
-    AMQP Exchange Name [OPTIONAL, default value: `water-usage-forecasts`]
+    Exchange Name
     
-    The name of the AMQP exchange which this service listens on for new messages
+    The name of the exchange this service will bind. The bound exchange will be used to consume
+    messages from other microservices
     """
     
-    class Config:
-        """Configuration of the AMQP connection settings"""
-        
-        env_file = '.amqp.env'
-        """The location of the environment file from which the settings may be read"""
+    exchange_type: pika.exchange_type.ExchangeType = pydantic.Field(
+        default=pika.exchange_type.ExchangeType.fanout,
+        alias='AMQP_EXCHANGE_TYPE',
+        title='Exchange Type',
+        description='The type of the specified exchange',
+        env='AMQP_EXCHANGE_TYPE'
+    )
+    """
+    Exchange Type
     
+    The type of exchange this service will bind itself to.
+    
+    **Important:** If the exchange already exists the supplied exchange type needs to match the
+    one with which the exchange has been created.
+    
+    Available values:
+       * direct
+       * fanout (default)
+       * headers
+       * topic
+    """
+
+    class Config:
+        """Configuration of the service settings"""
+    
+        env_file = 'env\\.amqp.env'
+        """Allow loading the values for the service settings from the specified file"""
