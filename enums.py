96d38354da58c85c5adefcf56e2ae9de95444c7e
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-04-22 10:02:58 +0200

    Moved enums to a different file and renamed them

---
+++ enums.py
@@ -0,0 +1,29 @@
+"""Enumerations for the forecasts"""
+import enum
+import settings
+
+
+class ForecastModel(str, enum.Enum):
+    LINEAR = "linear"
+    POLYNOMIAL = "polynomial"
+    LOGARITHMIC = "logarithmic"
+
+
+class ForecastGranularity(str, enum.Enum):
+    MUNICIPAL = "municipal"
+    DISTRICT = "district"
+
+
+class ErrorReasons(tuple, enum.Enum):
+    __service__ = settings.ServiceSettings()
+
+    INSUFFICIENT_DATA = (__service__.name + ".INSUFFICIENT_DATA", "Insufficient Data for forecast")
+    DATABASE_CONNECTION_ERROR = (
+        __service__.name + ".DB_CONNECTION_ERROR",
+        "Database Connection Error",
+    )
+    DATABASE_QUERY_ERROR = (__service__.name + ".DB_QUERY_ERROR", "Database Query Error")
+    OBJECT_NOT_FOUND_ERROR = (
+        __service__.name + ".QUERY_RETURNED_NULL",
+        "Database Returned Null Unexpectedly",
+    )
