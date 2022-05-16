03bf466bca3da92c0efeff5ea91569a76a8a5cbe
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-05-16 13:22:59 +0200

    Fixed indexing error in other forecast calculation methods

--- functions.py
+++ functions.py
@@ -51,7 +51,7 @@ def _run_forecast(
     elif model is enums.ForecastModel.POLYNOMIAL:
         curve = numpy.polynomial.Polynomial.fit(data_x_axis, data_y_axis, deg=2)
         all_calculated_values = curve(forecast_x_axis).tolist()
-        calculated_reference_values = all_calculated_values[: (len(data_x_axis) - 1)]
+        calculated_reference_values = all_calculated_values[: len(data_x_axis)]
         calculated_forecast_values = all_calculated_values[len(data_x_axis) :]
         forecast_score = sklearn.metrics.r2_score(usages, calculated_reference_values)
         equation = str(curve)
@@ -71,7 +71,7 @@ def _run_forecast(
     elif model is enums.ForecastModel.LOGARITHMIC:
         curve = numpy.polynomial.Polynomial.fit(numpy.log(data_x_axis), data_y_axis, deg=1)
         all_calculated_values = curve(forecast_x_axis).tolist()
-        calculated_reference_values = all_calculated_values[: (len(data_x_axis) - 1)]
+        calculated_reference_values = all_calculated_values[: len(data_x_axis)]
         calculated_forecast_values = all_calculated_values[len(data_x_axis) :]
         forecast_score = sklearn.metrics.r2_score(usages, calculated_reference_values)
         equation = str(curve)
