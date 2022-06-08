58e7842aba1d34b26b75d13e3204a1c7827a06c4
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-06-08 15:21:18 +0200

    Fixed wring slicing of fields

--- functions.py
+++ functions.py
@@ -35,8 +35,8 @@ def _run_forecast(
     if model is enums.ForecastModel.LINEAR:
         curve = numpy.polynomial.Polynomial.fit(data_x_axis, data_y_axis, deg=1)
         all_calculated_values = curve(forecast_x_axis).tolist()
-        calculated_reference_values = all_calculated_values[: len(data_x_axis)]
-        calculated_forecast_values = all_calculated_values[len(data_x_axis) :]
+        calculated_reference_values = all_calculated_values[:len(data_x_axis)]
+        calculated_forecast_values = all_calculated_values[len(data_x_axis):]
         forecast_score = sklearn.metrics.r2_score(usages, calculated_reference_values)
         equation = str(curve)
         forecast_result = {
@@ -54,8 +54,8 @@ def _run_forecast(
     elif model is enums.ForecastModel.POLYNOMIAL:
         curve = numpy.polynomial.Polynomial.fit(data_x_axis, data_y_axis, deg=2)
         all_calculated_values = curve(forecast_x_axis).tolist()
-        calculated_reference_values = all_calculated_values[: len(data_x_axis)]
-        calculated_forecast_values = all_calculated_values[len(data_x_axis) :]
+        calculated_reference_values = all_calculated_values[:len(data_x_axis)]
+        calculated_forecast_values = all_calculated_values[len(data_x_axis):]
         forecast_score = sklearn.metrics.r2_score(usages, calculated_reference_values)
         equation = str(curve)
         forecast_result = {
@@ -74,8 +74,8 @@ def _run_forecast(
     elif model is enums.ForecastModel.LOGARITHMIC:
         curve = numpy.polynomial.Polynomial.fit(numpy.log(data_x_axis), data_y_axis, deg=1)
         all_calculated_values = curve(forecast_x_axis).tolist()
-        calculated_reference_values = all_calculated_values[: len(data_x_axis)]
-        calculated_forecast_values = all_calculated_values[len(data_x_axis) :]
+        calculated_reference_values = all_calculated_values[:len(data_x_axis)]
+        calculated_forecast_values = all_calculated_values[len(data_x_axis):]
         forecast_score = sklearn.metrics.r2_score(usages, calculated_reference_values)
         equation = str(curve)
         forecast_result = {
